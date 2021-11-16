f102043401289ceb981ae90ec4eda3780b98a3b7
mosehkerubo <mosehkerubo@gmail.com>
2021-11-16 10:53:35 +0300

    the nav-bar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import Badge from '@mui/material/Badge';
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+// import LocalMallOutlinedIcon from '@mui/icons-material/Mail';
+
+
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <div className="nav-wrapper">
+      <div className="left">
+<span className="lang">en</span>
+
+
+<div className="search-container">
+<input type="text" className="input" placeholder="search product"/>
+<SearchOutlinedIcon/>
+</div>
+
+
+      </div>
+
+
+
+
+      <div className="center">
+        <h1>Magoso Enterprise.</h1>
+      </div>
+
+      <div className="right">
+<ul>
+  <li><a href="#">Login</a></li>
+  <li><a href="#">Register</a></li>
+  <Badge badgeContent={4} color="secondary">
+        <LocalMallOutlinedIcon color="action" />
+      </Badge>
+</ul>
+      </div>
+
+    </div>
+  );
+};
+
+export default Nav;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Announcement from "../components/Announcements/Announcements";
+import Nav from "../components/Nav/Nav";
 
 const Home=()=>{
     return(
+        <div>
     <Announcement />
+    <Nav />
+    </div>
         )
 }
 
