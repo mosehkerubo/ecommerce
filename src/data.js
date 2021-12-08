4c3662eb05f4535b1b96cdebe9e8fc2db24229e8
mosehkerubo <mosehkerubo@gmail.com>
2021-12-08 10:42:25 +0300

    made changes on app .js by removing the slash so that dlides can display

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const slides=[
 {
     id:1,
-    img:"/Images/orange.jpeg",
+    img:"Images/orange.jpeg",
     title:"HOLIDAY OFFERS",
     des:"Grab Our Latest December Offers",
     bg:"#B9FFFC"
@@ -12,7 +12,7 @@ export const slides=[
     
         {
             id:2,
-            img:"/Images/bread-lady.jpeg",
+            img:"Images/bread-lady.jpeg",
             title:"CLEARANCE SALE",
             des:"Here Comes Our Final ProductsFor The Year",
             bg:"#A3D8F4"
@@ -25,7 +25,7 @@ export const slides=[
     
             {
                 id:3,
-                img:"/Images/gloceries.jpeg",
+                img:"Images/gloceries.jpeg",
                 title:"NEW ARRIVALS",
                 des:"Dont Afford to Miss Yourself Quality products That Have Arrived in The Market",
                 bg:"#F5FDB0"
